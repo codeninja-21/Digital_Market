14df448ae94cea48c9280132060ce09062eb8212
afif.dev <salah7server@gmail.com>
2025-07-02 16:31:20 +0700

    initial

--- Digital_Market/Form1.Designer.cs
+++ Digital_Market/Form1.Designer.cs
@@ -39,7 +39,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.button1.Location = new System.Drawing.Point(507, 99);
+            this.button1.Location = new System.Drawing.Point(152, 34);
             this.button1.Name = "button1";
             this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.button1.Size = new System.Drawing.Size(75, 23);
